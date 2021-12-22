9bfa735c5a818867f51cd2dc9896ef53830a3073
limhyni <ghlduf114@gmail.com>
2021-12-22 14:47:20 +0900

    triangle Area

--- main.cpp
+++ main.cpp
@@ -7,6 +7,20 @@ int main()
 	
 	cout << "HelloWorld" << endl;
 
+	int Hight = 0;
+	int Base = 0;
+	float Area = 0.0f;
+
+	cout << "³ôÀÌ" << endl;
+	cin >> Hight;
+	cout << "¹Øº¯" << endl;
+	cin >> Base;
+
+	Area = static_cast<float>(Hight) * static_cast<float>(Base) / 2;
+
+	cout << "³ÐÀÌ" << endl;
+	cout << Area << endl;
+
 	return 0;
 
 }
